9a2f5e42fd60e5bdc093df029838663b1ebf3603
Chase Lambert <theicfire@gmail.com>
2015-03-17 02:23:52 -0700

    Ability to turn graph on/off

--- client/graph.js
+++ client/graph.js
@@ -1,4 +1,5 @@
 Session.set('soundAlarmOn', false);
+Session.set('graphOn', false);
 var alarmCount = -1;
 
 Template.Graph.helpers({
@@ -19,6 +20,15 @@ Template.Graph.helpers({
       var ret = Session.get('soundAlarmOn');
       console.log('return ', ret);
       return ret;
+  },
+  graphOn: function() {
+      return Session.get('graphOn');
+  },
+  graphDisplay: function() {
+      if (Session.get('graphOn')) {
+          return '';
+      }
+      return 'display:none';
   }
 });
 
@@ -48,6 +58,9 @@ Template.Graph.events({
   "click .ttsPretextButton": function (event) {
       Meteor.call('sendMsg', event.target.innerHTML);
       Meteor.call('ttsWaiting');
+  },
+  "change #graphOn": function (event) {
+      Session.set('graphOn', event.target.checked);
   }
 });
 
@@ -58,114 +71,119 @@ Router.route('/graph', function () {
 });
 
 var doneFirst = false;
-Template.Graph.created = function () {
-    var margin = {top: 20, right: 20, bottom: 30, left: 50},
-        width = 1500 - margin.left - margin.right,
-        height = 800 - margin.top - margin.bottom;
-    var x = d3.time.scale()
-        .range([0, width]);
+var margin = {top: 20, right: 20, bottom: 30, left: 50},
+    width = 1500 - margin.left - margin.right,
+    height = 800 - margin.top - margin.bottom;
+var x = d3.time.scale()
+    .range([0, width]);
 
-    var y = d3.scale.linear()
-        .range([height, 0]);
+var y = d3.scale.linear()
+    .range([height, 0]);
 
-    var xAxis = d3.svg.axis()
-        .scale(x)
-        .orient("bottom");
+var xAxis = d3.svg.axis()
+    .scale(x)
+    .orient("bottom");
 
-    var yAxis = d3.svg.axis()
-        .scale(y)
-        .orient("left");
+var yAxis = d3.svg.axis()
+    .scale(y)
+    .orient("left");
 
-    var lineX = d3.svg.line()
-        .x(function(d) { return x(d.createdAt); })
-        .y(function(d) { return y(d.x); });
-    var lineY = d3.svg.line()
-        .x(function(d) { return x(d.createdAt); })
-        .y(function(d) { return y(d.y); });
-    var lineZ = d3.svg.line()
-        .x(function(d) { return x(d.createdAt); })
-        .y(function(d) { return y(d.z); });
+var lineX = d3.svg.line()
+    .x(function(d) { return x(d.createdAt); })
+    .y(function(d) { return y(d.x); });
+var lineY = d3.svg.line()
+    .x(function(d) { return x(d.createdAt); })
+    .y(function(d) { return y(d.y); });
+var lineZ = d3.svg.line()
+    .x(function(d) { return x(d.createdAt); })
+    .y(function(d) { return y(d.z); });
 
+Template.Graph.rendered = function () {
     if (!doneFirst) {
         doneFirst = true;
-        var svg = d3.select("body").append("svg")
+        console.log('adding svg stuff');
+        var svg = d3.select("#graph")
             .attr("width", width + margin.left + margin.right)
             .attr("height", height + margin.top + margin.bottom)
           .append("g")
             .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
     }
+};
 
+Template.Graph.created = function () {
     Tracker.autorun(function () {
-        //var data = Accels.find({createdAt: {$gt: new Date(new Date().getTime() - 1000 * 120)}});
-        var batchAccelsRaw = BatchAccels.find().map(function(x) {
-                    return JSON.parse(x.accelsJson);
-                });
-        var batchAccels = [];
-        console.log('raw', batchAccelsRaw);
-        var curTime = new Date();
-        for (var i = 0; i < batchAccelsRaw.length; i++) {
-            //console.log('length', batchAccelsRaw[i].length);
-            for (var j = 0; j < batchAccelsRaw[i].length; j++) {
-                if (curTime - new Date(batchAccelsRaw[i][j][3]) < 20000) {
-                    batchAccels.push({x: batchAccelsRaw[i][j][0],
-                                      y: batchAccelsRaw[i][j][1],
-                                      z: batchAccelsRaw[i][j][2],
-                                      createdAt: new Date(batchAccelsRaw[i][j][3])});
+        if (Session.get('graphOn')) {
+            //var data = Accels.find({createdAt: {$gt: new Date(new Date().getTime() - 1000 * 120)}});
+            var batchAccelsRaw = BatchAccels.find().map(function(x) {
+                        return JSON.parse(x.accelsJson);
+                    });
+            var batchAccels = [];
+            console.log('raw', batchAccelsRaw);
+            var curTime = new Date();
+            for (var i = 0; i < batchAccelsRaw.length; i++) {
+                //console.log('length', batchAccelsRaw[i].length);
+                for (var j = 0; j < batchAccelsRaw[i].length; j++) {
+                    if (curTime - new Date(batchAccelsRaw[i][j][3]) < 20000) {
+                        batchAccels.push({x: batchAccelsRaw[i][j][0],
+                                          y: batchAccelsRaw[i][j][1],
+                                          z: batchAccelsRaw[i][j][2],
+                                          createdAt: new Date(batchAccelsRaw[i][j][3])});
+                    }
                 }
             }
-        }
-        var data = batchAccels;
-        console.log('now have', batchAccels);
-        //var row = AllAccels.findOne();
-        //if (!row) {
-            //return;
-        //}
-        //var data = [];
-        //// Important I look at the "times" length and not xs or something, because that's the last
-        //// array that is updated in mongo
-        //var curTime = new Date();
-        //for (var i = 0; i < row.times.length; i++) {
-            ////if (curTime - new Date(row.times[i]) < 120000) {
-                //data.push({x: parseFloat(row.xs[i]), y: parseFloat(row.ys[i]), z: parseFloat(row.zs[i]), createdAt: new Date(row.times[i])});
-            ////}
-        //}
-        //console.log('vs data', data);
+            var data = batchAccels;
+            console.log('now have', batchAccels);
+            //var row = AllAccels.findOne();
+            //if (!row) {
+                //return;
+            //}
+            //var data = [];
+            //// Important I look at the "times" length and not xs or something, because that's the last
+            //// array that is updated in mongo
+            //var curTime = new Date();
+            //for (var i = 0; i < row.times.length; i++) {
+                ////if (curTime - new Date(row.times[i]) < 120000) {
+                    //data.push({x: parseFloat(row.xs[i]), y: parseFloat(row.ys[i]), z: parseFloat(row.zs[i]), createdAt: new Date(row.times[i])});
+                ////}
+            //}
+            //console.log('vs data', data);
 
-        var maxY = 40;// d3.max(data, function (d) {return Math.max(d.x, d.y, d.z)});
-        var minY = -40; //d3.min(data, function (d) {return Math.min(d.x, d.y, d.z)});
+            var maxY = 40;// d3.max(data, function (d) {return Math.max(d.x, d.y, d.z)});
+            var minY = -40; //d3.min(data, function (d) {return Math.min(d.x, d.y, d.z)});
 
-        x.domain(d3.extent(data, function(d) { return d.createdAt; }));
-        y.domain([minY, maxY]);
+            x.domain(d3.extent(data, function(d) { return d.createdAt; }));
+            y.domain([minY, maxY]);
 
-        var svg = d3.select('svg g');
-        svg.html('');
-        svg.append("g")
-          .attr("class", "x axis")
-          .attr("transform", "translate(0," + height + ")")
-          .call(xAxis);
+            var svg = d3.select('svg g');
+            svg.html('');
+            svg.append("g")
+              .attr("class", "x axis")
+              .attr("transform", "translate(0," + height + ")")
+              .call(xAxis);
 
-        svg.append("g")
-          .attr("class", "y axis")
-          .call(yAxis)
-        .append("text")
-          .attr("transform", "rotate(-90)")
-          .attr("y", 6)
-          .attr("dy", ".71em")
-          .style("text-anchor", "end")
-          .text("Price ($)");
+            svg.append("g")
+              .attr("class", "y axis")
+              .call(yAxis)
+            .append("text")
+              .attr("transform", "rotate(-90)")
+              .attr("y", 6)
+              .attr("dy", ".71em")
+              .style("text-anchor", "end")
+              .text("Price ($)");
 
-        svg.append("path")
-          .datum(data)
-          .attr("class", "lineX")
-          .attr("d", lineX);
-        svg.append("path")
-          .datum(data)
-          .attr("class", "lineY")
-          .attr("d", lineY);
-        svg.append("path")
-          .datum(data)
-          .attr("class", "lineZ")
-          .attr("d", lineZ);
+            svg.append("path")
+              .datum(data)
+              .attr("class", "lineX")
+              .attr("d", lineX);
+            svg.append("path")
+              .datum(data)
+              .attr("class", "lineY")
+              .attr("d", lineY);
+            svg.append("path")
+              .datum(data)
+              .attr("class", "lineZ")
+              .attr("d", lineZ);
+      }
     });
 
     Tracker.autorun(function() {
